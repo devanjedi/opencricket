18bcd03ec27589f4afa3574bcb8706f9c39fafb2
Devanshu Mehta <skywalker@galaxyfaraway.com>
2014-04-23 16:50:45 -0400

    Fixed an issue where it would read in everything as a date

--- src/ci/guruparser.py
+++ src/ci/guruparser.py
@@ -6,17 +6,19 @@
 #Change f= to filename of resulting CSV
 import pandas
     
-url="http://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;opposition=8;team=2;template=results;type=bowling;view=innings;"
+url="http://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;opposition=3;team=6;template=results;type=bowling"
 pageNum=1
 nextPage=1
 f=open('../../testdata/guru.csv', 'a')
 guruFrame = pandas.DataFrame()
 while(nextPage):
-    dfs=pandas.read_html(url+';page='+str(pageNum), attrs={'class':'engineTable'})[2]
+    dfs=pandas.read_html(url+';page='+str(pageNum), attrs={'class':'engineTable'},infer_types=False)[2]
     guruFrame=guruFrame.append(dfs)
     if len(dfs.index)<50:
         nextPage=0
     else:
         nextPage=1
+    print "page %d done..." % pageNum
     pageNum=pageNum+1
+print guruFrame
 guruFrame.to_csv(f)
