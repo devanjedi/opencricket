a36f603985f9c2b08a61f8919cddeacd9602b7aa
Devanshu Mehta <skywalker@galaxyfaraway.com>
2014-04-26 22:42:55 -0400

    Added some help to cricsheet sample scripts

--- src/cricsheet/chaser.py
+++ src/cricsheet/chaser.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
+#This script will check all the cricsheet yaml files in directory
+#specified by variable 'dirtocheck'.
+#It will count how many matches were won by chasing team, and how 
+# many were won by side batting first and print results.
 
 import yaml, os
-dirtocheck="cricsheetIPL"
+dirtocheck="cricsheetT20I"
 chaser=0
 notchaser=0
 for root, _, files in os.walk(dirtocheck):
--- src/cricsheet/outcomes.py
+++ src/cricsheet/outcomes.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
+#This script will check all the cricsheet yaml files in directory
+#specified by variable 'dirtocheck'.
+#It will count how many matches were won by toss-winning team, and how
+# many were won by side losing the toss and print results.
 
 import yaml, os
-dirtocheck="cricsheetIPL"
+dirtocheck="cricsheetT20I"
 toss=0
 nottoss=0
 for root, _, files in os.walk(dirtocheck):
