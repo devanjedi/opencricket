b9b70966e1deef89a52424e6c2c2e911fc049b11
Devanshu Mehta <skywalker@galaxyfaraway.com>
2014-04-28 21:41:52 -0400

    added function to cricsheet helper

--- src/cricsheet/cricsheet.py
+++ src/cricsheet/cricsheet.py
@@ -4,3 +4,17 @@
 
 def getTeams(yamldoc):
 	return yamldoc['info']['teams']
+	
+def getMatchWinner(yamldoc):
+	matchoutcome= yamldoc['info']['outcome']
+	if 'winner' in matchoutcome:
+		return matchoutcome['winner']
+	elif 'eliminator' in matchoutcome:
+		return matchoutcome['eliminator']
+	else:
+		return['None']
+
+def getTossWinner(yamldoc):
+	return yamldoc['info']['toss']['winner']
+	
+
