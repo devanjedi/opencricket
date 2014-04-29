46a911286f0070f804223cdf4fbfde82172d317a
Devanshu Mehta <skywalker@galaxyfaraway.com>
2014-04-28 23:36:07 -0400

    Added a few nifty functions to cricsheet helper

--- src/cricsheet/cricsheet.py
+++ src/cricsheet/cricsheet.py
@@ -17,4 +17,11 @@ def getMatchWinner(yamldoc):
 def getTossWinner(yamldoc):
 	return yamldoc['info']['toss']['winner']
 	
+def getBatsmen(yamldoc):
+	for delivery in  yamldoc['innings'][0]['1st innings']['deliveries']:
+		for info in delivery:
+			print info
 
+def runsBetween(yamldoc, start, end):
+	print yamldoc['innings'][0]['1st innings']['deliveries'][start-1][float(start)/10]['runs']['total']
+	print  yamldoc['innings'][0]['1st innings']['deliveries'][end-1][float(end)/10]['runs']['total']
